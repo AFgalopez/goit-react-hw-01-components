615ba6f564339f4385a561bb11a066f45a3340ef
AFgalopez <afgalopez@gmail.com>
2024-02-06 21:13:42 -0500

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import React from 'react';
+import Profile from '../components/profile/Profile';
+import Statistics from '../components/statistics/Statistics';
+import FriendList from '../components/friendList/FriendList';
+import Transactions from '../components/transactions/Transactions';
+import user from '../data/user';
+import data from '../data/data-statistics';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>React Homework</h1>
+      <h2>Profile</h2>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+      <h2>Statistics</h2>
+      <Statistics title="Upload stats" stats={data}></Statistics>
+      <h2>Friend List</h2>
+      <FriendList friends={friends}></FriendList>
+      <h2>Transactions</h2>
+      <Transactions items={transactions}></Transactions>
+    </>
   );
 };
+
+export default App;
